f02ebbccf5e92d01e15417262c8e0d3454cf8400
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-16 19:32:33 +0900

    os를 활용해 여러 정보 얻기

--- ch02_Test05.js
+++ ch02_Test05.js
@@ -12,3 +12,9 @@ console.log('모듈 분리 후 - calc.add 함수 호출 결과 %d', calc.add(10,
 var calc2 = require("./calc2");
 console.log('모듈 분리 후 - calc.add 함수 호출 결과 %d', calc2.add(10, 10));
 
+var os = require('os');
+console.log("시스템의 메모리 : %d, / %d", os.freemem(), os.totalmem());
+console.dir("시스템의 cpu 정보\n")
+console.dir(os.cpus());
+console.log("시스템의 네트워크 인터페이스정보 \n");
+console.log(os.networkInterfaces());
