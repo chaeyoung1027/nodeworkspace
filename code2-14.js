893e6783a43c94964717f3ad4787100fd8571fc5
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-21 18:58:20 +0900

    + 와 ` 사용해 출력해보기

---
+++ code2-14.js
@@ -0,0 +1,11 @@
+let pi = 3.14159265;
+let radius = 10; //반지름
+
+console.log("원의 둘레 : "+(2*pi*radius));
+console.log("원의 넓이 : "+(2*radius*radius));
+
+console.log(`원의 둘레 : ${ 2*pi*radius }`);
+console.log(`원의 넓이 : ${ 2*radius*radius }`);
+
+console.log("반지름 : "+radius +"입니다.");
+console.log(`반지름 : ${radius}  입니다.`);
