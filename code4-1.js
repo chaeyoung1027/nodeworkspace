3e8c94faeab25b4caa83044e61a256f1ff759f74
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-21 19:40:55 +0900

    if문, 배열 배우기

---
+++ code4-1.js
@@ -0,0 +1,11 @@
+//let i = 0;
+// for(let i = 0; i<1000; i++){
+//     console.log("출력!");
+// }
+
+let array = [52, 352, '안녕', '끝!', true, false];
+array[0] = 0;   //값 변경
+
+for(let i = 0; i<5; i++){
+    console.log(array[i]);
+}
