640d8972bc6efed227fe60ab8494c60920993937
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-23 19:09:52 +0900

    두 수를 곱해주는 multiple함수와 한 말을 출력해주는 print함수 작성

---
+++ returnFunc.js
@@ -0,0 +1,12 @@
+function multiple(x, y){
+    return x*y;
+}
+
+function print(message){
+    console.log(`${message}라고 말했습니다.`);
+}
+
+print("안녕하세요!");
+print("자바스크립트 공부!!")
+console.log(52, 267);
+console.log(32, 632);
