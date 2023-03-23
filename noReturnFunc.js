1e6f383b42f336d9d66c598cb3d891c8e3d57f2c
chaeyoung1027 <rnldyal1027@naver.com>
2023-03-23 19:24:41 +0900

    매개변수에 바로 초기화

--- noReturnFunc.js
+++ noReturnFunc.js
@@ -1,9 +1,6 @@
 //함수를 선언합니다.
-function print(name, count){
-    if(typeof(count) == "undefined"){
-        count+=1;
-    }
-    console.log(`${name}이/가 ${count}에 있습니다.`);
+function print(name, count = 1){
+    console.log(`${name}이/가 ${count}개 있습니다.`);
 }
 
 //함수를 호출합니다.
