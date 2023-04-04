425250377dbe6e46038f13c72afa94327edfdb98
chaeyoung1027 <rnldyal1027@naver.com>
2023-04-04 20:04:14 +0900

    url사용

---
+++ code 9-20.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+let url = 'https://www.e-mirim.hs.kr/main.do';
+request(url, (err, response, body)=>{
+    if(err){
+        console.log("request error!");
+        console.log(err);
+    }else{
+        console.log(body);
+    }
+})
