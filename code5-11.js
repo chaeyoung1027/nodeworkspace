812955b6a61cc9faa6045f1ba495c01e80878841
chaeyoung1027 <rnldyal1027@naver.com>
2023-04-04 19:34:29 +0900

    메모장 callback하기

--- code5-11.js
+++ code5-11.js
@@ -4,3 +4,11 @@ fs.readFile('test.txt', (error, file)=>{
     console.log(file);
     console.log(file.toString());
 });
+
+async.parallel([
+    (callback) => {fs.readFile('a.txt', callback);},
+    (callback) => {fs.readFile('b.txt', callback);},
+    (callback) => {fs.readFile('c.txt', callback);},
+],(err, data)=>{
+    console.log(result);
+})
