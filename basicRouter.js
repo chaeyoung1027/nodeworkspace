66a6156658eab754b02650a3ae4729cd27af15c2
chaeyoung1027 <rnldyal1027@naver.com>
2023-04-06 19:47:56 +0900

    에러 수정

--- basicRouter.js
+++ basicRouter.js
@@ -4,10 +4,10 @@ const express = require('express');
 //서버를 생성합니다.
 const app = express();
 
-//request이벤트 리스너를 설정합니다.
+//request 이벤트 리스너를 설정합니다.
 app.get('/page/:id', (request, response)=>{
     //토큰을 꺼냅니다.
-    const id = request.parmas.id;
+    const id = request.params.id;
 
     //응답합니다. 
     response.send(`<h1>${id} Page</h1>`);
@@ -15,5 +15,5 @@ app.get('/page/:id', (request, response)=>{
 
 //서버를 실행합니다.
 app.listen(3000, ()=>{
-    console.log('Server running at http://10.96.124.97:3000/');
+    console.log('Server running at http://10.96.124.97:3000/page/:id');
 });
