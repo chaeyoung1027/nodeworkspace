0196faacc841976666d0dd20396dc2dab8bc1006
chaeyoung1027 <rnldyal1027@naver.com>
2023-04-13 20:05:27 +0900

    commit

--- code10-9.js
+++ code10-9.js
@@ -1,25 +1,33 @@
-const bodyParser = require('body-parser');
-let express = require('express');
-
+const express = require('express');  
+const bodyParser = require('body-parser');  // Import bodyParser middleware
 let app = express();
-//정적파일제공하는 폴더
-app.use(express.static('public'));
-app.use(bodyParser.json);
 
-app.get('/', (request, response)=>{
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Parse JSON data from request body
+app.use(bodyParser.json());
+
+// Parse URL-encoded form data from request body
+app.use(express.urlencoded({extended:false}));
+
+// Route handler for all GET requests
+app.get('*', (request, response)=>{
     let output = '';
-    output+='<form method = "post">';
-    output += '<input type = "text" name = "id" />'
-    output += '<input type = "text" name = "passwd" />'
-    output += '<input type = "submit"    />'
+    output+='<form method="post">';
+    output += '<input type="text" name="id" />'
+    output += '<input type="text" name="passwd" />'
+    output += '<input type="submit" />'
     output+='</form>'
     response.send(output);
 });
+
+// Route handler for POST request to root URL
 app.post('/', (request, response) =>{
     console.log(request.body);
-    console.send(request.body);
+    response.send(request.body);
 });
 
 app.listen(3000, ()=>{
-    console.log('setver starting 3000 port');
+    console.log('Server starting on port 3000');
 });
